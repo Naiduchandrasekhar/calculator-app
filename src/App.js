4251d18e90a03fcf6259ec3d81f75b721dc45baa
ChanduSekhar-Naidu <110819177+Naiduchandrasekhar@users.noreply.github.com>
2023-03-03 16:32:22 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
 import './App.css';
 
+const cal = ['/', '*', '+', '-', 1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+
 function App() {
+  const [input, setInput] = useState("")
+  const [result, setResult] = useState(0)
+
+  const handleChangeValue = (e) => {
+    setInput(e.target.value)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Calculator App</h1>
+      <input type="text" value={input} onChange={handleChangeValue} />
+      <br />
+      <button onClick={() => setResult(eval(input))}>Result</button>
+      <h3>Result is {result}</h3>
+
+      <div className='container'>
+        <div className='calculatorBox'>
+          <button  className='button clearBtn' onClick={() => {setInput('') 
+        setResult(0)}} >Clear</button>
+          {cal.map(c => {
+            return <button className='button' onClick={() => setInput(input + c)}>{c}</button>
+          })}
+        </div>
+      </div>
     </div>
+
   );
 }
 
